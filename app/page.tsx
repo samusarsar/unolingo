2cbed33915dd1cdfb70f22e4e86e46c773af2eb1
samu.sar <samuil.sarandev@gmail.com>
2024-03-27 09:52:52 +0200

    Add: add shadcn-ui button component to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
 	return (
 		<div>
 			<p className='text-green-500'>Hello Lingo!</p>
+			<Button>Click me</Button>
 		</div>
 	);
 }
