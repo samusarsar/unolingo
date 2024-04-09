1c3808413b51b132146a2b070568f88805122132
samu.sar <samuil.sarandev@gmail.com>
2024-04-09 12:51:55 +0300

    Fix: updated /learn layout and page to integrate new components

--- app/(main)/learn/layout.tsx
+++ app/(main)/learn/layout.tsx
@@ -8,7 +8,9 @@ const MainLayout = ({ children }: BaseProps) => {
 			<MobileHeader />
 			<Sidebar className='hidden lg:flex' />
 			<main className='lg:pl-[256px] h-full pt-[50px] lg:pt-0'>
-				<div className='bg-red-500 h-full'>{children}</div>
+				<div className='h-full max-w-[1056px] mx-auto pt-6'>
+					{children}
+				</div>
 			</main>
 		</>
 	);
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,17 @@
+import { StickyWrapper } from '@/components/sticky-wrapper';
+import { FeedWrapper } from '@/components/feed-wrapper';
+import { Header } from './_components/header';
+import { UserProgress } from '@/components/user-progress';
+
 const LearnPage = () => {
-    return ( <div>Learn</div> );
-}
- 
+	return (
+		<div className='flex flex-row-reverse gap-[48px] px-6'>
+			<StickyWrapper><UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }} hearts={5} points={100} hasActiveSubscription={false} /></StickyWrapper>
+			<FeedWrapper>
+				<Header title='Spanish' />
+			</FeedWrapper>
+		</div>
+	);
+};
+
 export default LearnPage;
