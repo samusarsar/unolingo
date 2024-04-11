f3506b5c63758eb02260b826db1608918bba71bc
samu.sar <samuil.sarandev@gmail.com>
2024-04-11 11:24:36 +0300

    Add: added learn page guard when userProgress has no activeCourse

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,20 @@
+import { redirect } from 'next/navigation';
+
+import { Header } from './_components/header';
 import { StickyWrapper } from '@/components/sticky-wrapper';
 import { FeedWrapper } from '@/components/feed-wrapper';
-import { Header } from './_components/header';
 import { UserProgress } from '@/components/user-progress';
+import { getUserProgress } from '@/db/queries';
 
-const LearnPage = () => {
+const LearnPage = async () => {
+	const userProgressData = getUserProgress();
+
+	const [userProgress] = await Promise.all([userProgressData]);
+
+	if (!userProgress || !userProgress.activeCourse) {
+		redirect('/courses');
+	}
+
 	return (
 		<div className='flex flex-row-reverse gap-[48px] px-6'>
 			<StickyWrapper><UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }} hearts={5} points={100} hasActiveSubscription={false} /></StickyWrapper>
