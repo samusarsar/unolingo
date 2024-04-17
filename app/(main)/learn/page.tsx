c13239ca9bb3510916af3631b606408c2de72a07
samu.sar <samuil.sarandev@gmail.com>
2024-04-17 15:25:07 +0300

    Add: added real userProgress data to courses

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -17,9 +17,9 @@ const LearnPage = async () => {
 
 	return (
 		<div className='flex flex-row-reverse gap-[48px] px-6'>
-			<StickyWrapper><UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }} hearts={5} points={100} hasActiveSubscription={false} /></StickyWrapper>
+			<StickyWrapper><UserProgress activeCourse={userProgress.activeCourse} hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={false} /></StickyWrapper>
 			<FeedWrapper>
-				<Header title='Spanish' />
+				<Header title={userProgress.activeCourse.title} />
 			</FeedWrapper>
 		</div>
 	);
--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,14 +1,12 @@
 import Link from 'next/link';
-
-import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { InfinityIcon } from 'lucide-react';
 
+import { courses } from '@/db/schema';
+import { Button } from '@/components/ui/button';
+
 type Props = {
-	activeCourse: {
-		title: string;
-		imageSrc: string;
-	};
+	activeCourse: typeof courses.$inferSelect;
 	hearts: number;
 	points: number;
 	hasActiveSubscription: boolean;
