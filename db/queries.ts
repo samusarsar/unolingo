acebc00587633c02f2cd05a472a1654d603e3591
samu.sar <samuil.sarandev@gmail.com>
2024-04-18 11:39:35 +0300

    Add: added getUnits query

--- db/queries.ts
+++ db/queries.ts
@@ -3,7 +3,7 @@ import { auth } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
 
 import db from '@/db/drizzle';
-import { courses, userProgress } from '@/db/schema';
+import { courses, units, userProgress } from '@/db/schema';
 
 export const getCourses = cache(async () => {
 	const data = await db.query.courses.findMany();
@@ -28,9 +28,54 @@ export const getUserProgress = cache(async () => {
 	return data;
 });
 
+export const getUnits = cache(async () => {
+	const userProgress = await getUserProgress();
+
+	if (!userProgress?.activeCourseId) {
+		return [];
+	}
+
+	const data = await db.query.units.findMany({
+		where: eq(units.courseId, userProgress.activeCourseId),
+		with: {
+			lessons: {
+				with: {
+					challenges: {
+						with: {
+							challengeProgress: true,
+						},
+					},
+				},
+			},
+		},
+	});
+
+	const normalizedData = data.map((unit) => {
+		const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+			const allCompletedChallenges = lesson.challenges.every(
+				(challenge) => {
+					return (
+						challenge.challengeProgress &&
+						challenge.challengeProgress.length > 0 &&
+						challenge.challengeProgress.every(
+							(progress) => progress.completed
+						)
+					);
+				}
+			);
+
+			return { ...lesson, completed: allCompletedChallenges };
+		});
+
+		return { ...unit, lessons: lessonsWithCompletedStatus };
+	});
+
+	return normalizedData;
+});
+
 export const getCourseById = cache(async (courseId: number) => {
 	const data = await db.query.courses.findFirst({
-		where: eq(courses.id, courseId)
+		where: eq(courses.id, courseId),
 		// TODO: populate units and lessons
 	});
 
