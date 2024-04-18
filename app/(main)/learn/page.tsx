c349dbb750bf0afd20e4427ca1747b012a77352f
samu.sar <samuil.sarandev@gmail.com>
2024-04-18 14:22:59 +0300

    Add: implemented initial unit and lesson components in Learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,15 +1,20 @@
 import { redirect } from 'next/navigation';
 
 import { Header } from './_components/header';
+import { Unit } from './_components/unit';
 import { StickyWrapper } from '@/components/sticky-wrapper';
 import { FeedWrapper } from '@/components/feed-wrapper';
 import { UserProgress } from '@/components/user-progress';
-import { getUserProgress } from '@/db/queries';
+import { getUnits, getUserProgress } from '@/db/queries';
 
 const LearnPage = async () => {
 	const userProgressData = getUserProgress();
+	const unitsData = getUnits();
 
-	const [userProgress] = await Promise.all([userProgressData]);
+	const [userProgress, units] = await Promise.all([
+		userProgressData,
+		unitsData,
+	]);
 
 	if (!userProgress || !userProgress.activeCourse) {
 		redirect('/courses');
@@ -17,9 +22,32 @@ const LearnPage = async () => {
 
 	return (
 		<div className='flex flex-row-reverse gap-[48px] px-6'>
-			<StickyWrapper><UserProgress activeCourse={userProgress.activeCourse} hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={false} /></StickyWrapper>
+			<StickyWrapper>
+				<UserProgress
+					activeCourse={userProgress.activeCourse}
+					hearts={userProgress.hearts}
+					points={userProgress.points}
+					hasActiveSubscription={false}
+				/>
+			</StickyWrapper>
 			<FeedWrapper>
 				<Header title={userProgress.activeCourse.title} />
+				{units.map((unit) => (
+					<div
+						key={unit.id}
+						className='mb-10'
+					>
+						<Unit
+							id={unit.id}
+							order={unit.order}
+							description={unit.description}
+							title={unit.title}
+							lessons={unit.lessons}
+							activeLesson={undefined}
+							activeLessonPercentage={0}
+						/>
+					</div>
+				))}
 			</FeedWrapper>
 		</div>
 	);
