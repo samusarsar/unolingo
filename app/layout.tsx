c5ab60c16a1cbb452902339da895f64de40f49bc
samu.sar <samuil.sarandev@gmail.com>
2024-04-24 12:02:29 +0300

    Add: implemented ExitModal on exiting lesson

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from '@/components/ui/sonner';
+import { ExitModal } from '@/components/modals/exit-modal';
 
 import './globals.css';
 
@@ -22,6 +23,7 @@ export default function RootLayout({
 			<html lang='en'>
 				<body className={font.className}>
 					<Toaster />
+					<ExitModal />
 					{children}
 				</body>
 			</html>
--- app/lesson/_components/header.tsx
+++ app/lesson/_components/header.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { InfinityIcon, X } from 'lucide-react';
 
 import { Progress } from '@/components/ui/progress';
+import { useExitModal } from '@/store/use-exit-modal';
 
 type Props = {
 	hearts: number;
@@ -14,26 +15,29 @@ export const Header = ({
 	percentage,
 	hasActiveSubscription,
 }: Props) => {
+	const { open } = useExitModal();
+
 	return (
 		<header className='lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full'>
 			<X
-                onClick={() => {}}
-                className='text-slate-500 hover:opacity-75 transition cursor-pointer'
-            />
-            <Progress value={percentage} />
-            <div className='text-rose-500 flex items-center font-bold'>
-                <Image 
-                    src='/heart.svg'
-                    height={28}
-                    width={28}
-                    alt='Heart'
-                    className='mr-2'
-                />
-                {hasActiveSubscription
-                    ? <InfinityIcon className='h-6 w-6 stroke-[3]' />
-                    : hearts
-                }
-            </div>
+				onClick={open}
+				className='text-slate-500 hover:opacity-75 transition cursor-pointer'
+			/>
+			<Progress value={percentage} />
+			<div className='text-rose-500 flex items-center font-bold'>
+				<Image
+					src='/heart.svg'
+					height={28}
+					width={28}
+					alt='Heart'
+					className='mr-2'
+				/>
+				{hasActiveSubscription ? (
+					<InfinityIcon className='h-6 w-6 stroke-[3]' />
+				) : (
+					hearts
+				)}
+			</div>
 		</header>
 	);
 };
