3ea48c198f60764d2a6ba181491875e41ee3c0b4
samu.sar <samuil.sarandev@gmail.com>
2024-04-25 12:24:09 +0300

    Add: added user-progress action to reduceHearts on mistaken challengeOption selection

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -3,10 +3,11 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { auth, currentUser } from '@clerk/nextjs';
-import db from '@/db/drizzle';
+import { and, eq } from 'drizzle-orm';
 
+import db from '@/db/drizzle';
 import { getCourseById, getUserProgress } from '@/db/queries';
-import { userProgress } from '@/db/schema';
+import { challenges, challengeProgress, userProgress } from '@/db/schema';
 
 export const upsertUserProgress = async (courseId: number) => {
 	const { userId } = await auth();
@@ -51,4 +52,62 @@ export const upsertUserProgress = async (courseId: number) => {
 	revalidatePath('/courses');
 	revalidatePath('/learn');
 	redirect('/learn');
+};
+
+export const reduceHearts = async (challengeId: number) => {
+	const { userId } = await auth();
+
+	if (!userId) {
+		throw new Error('Unauthorized');
+	}
+
+	const currentUserProgress = await getUserProgress();
+	// TODO: get user subscription
+
+	const challenge = await db.query.challenges.findFirst({
+		where: eq(challenges.id, challengeId),
+	});
+
+	if (!challenge) {
+		throw new Error('Challenge not found');
+	}
+
+	const lessonId = challenge.lessonId;
+
+	const existingChallengeProgress =
+		await db.query.challengeProgress.findFirst({
+			where: and(
+				eq(challengeProgress.userId, userId),
+				eq(challengeProgress.challengeId, challengeId)
+			),
+		});
+
+	const isPractice = !!existingChallengeProgress;
+
+	if (isPractice) {
+		return { error: 'practice' };
+	}
+
+	if (!currentUserProgress) {
+		throw new Error('User progress not found');
+	}
+
+	//TODO: handle subscription
+
+	if (currentUserProgress.hearts === 0) {
+		return { error: 'hearts' };
+	}
+
+	await db
+		.update(userProgress)
+		.set({
+			hearts: Math.max(currentUserProgress.hearts - 1, 0),
+		})
+		.where(eq(userProgress.userId, userId));
+
+	revalidatePath('/shop');
+	revalidatePath('/learn');
+	revalidatePath('/quests');
+	revalidatePath('/leaderboard');
+	revalidatePath(`/lesson/${lessonId}`);
 };
