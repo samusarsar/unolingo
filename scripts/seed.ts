f134f613cba5844723c0d1119206d5e68759362b
samu.sar <samuil.sarandev@gmail.com>
2024-04-26 12:32:03 +0300

    Fix: added challenges to second lesson in seed script to test lesson finish screen

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -177,6 +177,30 @@ const main = async () => {
 			},
 		]);
 
+		await db.insert(schema.challenges).values([
+			{
+				id: 4,
+				lessonId: 2,
+				type: 'SELECT',
+				order: 1,
+				question: 'Which one of these is "the man"?',
+			},
+			{
+				id: 5,
+				lessonId: 2,
+				type: 'ASSIST',
+				order: 2,
+				question: '"the man"',
+			},
+			{
+				id: 6,
+				lessonId: 2,
+				type: 'SELECT',
+				order: 3,
+				question: 'Which one of these is "the robot"?',
+			},
+		]);
+
 		console.log('Seeding completed');
 	} catch (error) {
 		console.error(error);
