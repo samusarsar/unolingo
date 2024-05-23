5e2f462a239ce49fd09c920a0fee07bf9759d7e9
samu.sar <samuil.sarandev@gmail.com>
2024-05-23 15:52:49 +0300

    Add: refactor constants into separate common file

---
+++ common/constants.ts
@@ -0,0 +1,16 @@
+export const POINTS_TO_REFILL = 10;
+
+export const quests = [
+	{
+		title: 'Earn 20 XP',
+		value: 20,
+	},
+	{
+		title: 'Earn 50 XP',
+		value: 50,
+	},
+	{
+		title: 'Earn 100 XP',
+		value: 100,
+	},
+];
