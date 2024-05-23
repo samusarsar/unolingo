c7f5fb3707e2ff423bc4100c213a316c01d04c36
samu.sar <samuil.sarandev@gmail.com>
2024-05-23 15:53:17 +0300

    Fix: fixed TODO in queries and added getTopTenUsers query for leaderboard page

--- db/queries.ts
+++ db/queries.ts
@@ -46,11 +46,16 @@ export const getUnits = cache(async () => {
 	}
 
 	const data = await db.query.units.findMany({
+		orderBy: (units, { asc }) => [asc(units.order)],
 		where: eq(units.courseId, userProgress.activeCourseId),
 		with: {
 			lessons: {
+				orderBy: (lessons, { asc }) => [asc(lessons.order)],
 				with: {
 					challenges: {
+						orderBy: (challenges, { asc }) => [
+							asc(challenges.order),
+						],
 						with: {
 							challengeProgress: {
 								where: eq(challengeProgress.userId, userId),
@@ -92,7 +97,16 @@ export const getUnits = cache(async () => {
 export const getCourseById = cache(async (courseId: number) => {
 	const data = await db.query.courses.findFirst({
 		where: eq(courses.id, courseId),
-		// TODO: populate units and lessons
+		with: {
+			units: {
+				orderBy: (units, { asc }) => [asc(units.order)],
+				with: {
+					lessons: {
+						orderBy: (lessons, { asc }) => [asc(lessons.order)],
+					},
+				},
+			},
+		},
 	});
 
 	return data;
@@ -232,4 +246,25 @@ export const getUserSubscription = cache(async () => {
 		...data,
 		isActive: !!isActive,
 	};
+});
+
+export const getTopTenUsers = cache(async () => {
+	const { userId } = await auth();
+
+	if (!userId) {
+		return [];
+	}
+
+	const data = await db.query.userProgress.findMany({
+		orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+		limit: 10,
+		columns: {
+			userId: true,
+			userName: true,
+			userImageSrc: true,
+			points: true,
+		},
+	});
+
+	return data;
 });
