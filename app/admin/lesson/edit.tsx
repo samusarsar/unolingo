8de27afa1018059dfdbe1105727e308f4fad12e0
samu.sar <samuil.sarandev@gmail.com>
2024-05-27 16:18:13 +0300

    Add: added lesson resource structures and api routes for lessons

---
+++ app/admin/lesson/edit.tsx
@@ -0,0 +1,36 @@
+import {
+	SimpleForm,
+	Edit,
+	TextInput,
+	ReferenceInput,
+	NumberInput,
+	required,
+} from 'react-admin';
+
+export const LessonEdit = () => {
+	return (
+		<Edit>
+			<SimpleForm>
+				<NumberInput
+					source='id'
+					validate={[required()]}
+					label='Id'
+				/>
+				<TextInput
+					source='title'
+					validate={[required()]}
+					label='Title'
+				></TextInput>
+				<ReferenceInput
+					source='unitId'
+					reference='units'
+				/>
+				<NumberInput
+					source='order'
+					validate={[required()]}
+					label='Order'
+				/>
+			</SimpleForm>
+		</Edit>
+	);
+};
---
+++ app/admin/lesson/list.tsx
@@ -0,0 +1,17 @@
+import { Datagrid, List, TextField, ReferenceField, NumberField } from 'react-admin';
+
+export const LessonList = () => {
+	return (
+		<List>
+			<Datagrid rowClick='edit'>
+				<NumberField source='id' />
+				<TextField source='title' />
+				<ReferenceField
+					source='unitId'
+					reference='units'
+				/>
+				<NumberField source='order' />
+			</Datagrid>
+		</List>
+	);
+};
