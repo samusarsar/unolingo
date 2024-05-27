748091fbbd95719ebb7ba062e70b62d7bf52df5b
samu.sar <samuil.sarandev@gmail.com>
2024-05-27 16:19:31 +0300

    Add: integrated all admin resources in admin app

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -7,6 +7,22 @@ import { CourseList } from './course/list';
 import { CourseCreate } from './course/create';
 import { CourseEdit } from './course/edit';
 
+import { UnitList } from './unit/list';
+import { UnitCreate } from './unit/create';
+import { UnitEdit } from './unit/edit';
+
+import { LessonList } from './lesson/list';
+import { LessonCreate } from './lesson/create';
+import { LessonEdit } from './lesson/edit';
+
+import { ChallengeList } from './challenge/list';
+import { ChallengeCreate } from './challenge/create';
+import { ChallengeEdit } from './challenge/edit';
+
+import { ChallengeOptionList } from './challengeOption/list';
+import { ChallengeOptionCreate } from './challengeOption/create';
+import { ChallengeOptionEdit } from './challengeOption/edit';
+
 const dataProvider = simpleRestProvider('/api');
 
 const App = () => {
@@ -15,9 +31,40 @@ const App = () => {
 			<Resource
 				name='courses'
 				list={CourseList}
-                create={CourseCreate}
-                edit={CourseEdit}
+				create={CourseCreate}
+				edit={CourseEdit}
 				recordRepresentation='title'
+			/>
+			<Resource
+				name='units'
+				list={UnitList}
+				create={UnitCreate}
+				edit={UnitEdit}
+				recordRepresentation='title'
+			/>
+			<Resource
+				name='lessons'
+				list={LessonList}
+				create={LessonCreate}
+				edit={LessonEdit}
+				recordRepresentation='title'
+			/>
+			<Resource
+				name='challenges'
+				list={ChallengeList}
+				create={ChallengeCreate}
+				edit={ChallengeEdit}
+				recordRepresentation='question'
+			/>
+			<Resource
+				name='challengeOptions'
+				list={ChallengeOptionList}
+				create={ChallengeOptionCreate}
+				edit={ChallengeOptionEdit}
+				recordRepresentation='text'
+				options={{
+					label: 'Challenge Options',
+				}}
 			/>
 		</Admin>
 	);
