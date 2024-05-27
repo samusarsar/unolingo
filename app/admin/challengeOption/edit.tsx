5f30ff56003985dec466ab54b85b7b20033c385f
samu.sar <samuil.sarandev@gmail.com>
2024-05-27 16:18:54 +0300

    Add: added admin resource structures and api routes for challengeOptions

---
+++ app/admin/challengeOption/edit.tsx
@@ -0,0 +1,15 @@
+import { SimpleForm, Edit, TextInput, ReferenceInput, NumberInput, BooleanInput, required } from 'react-admin';
+
+export const ChallengeOptionEdit = () => {
+    return (
+        <Edit>
+            <SimpleForm>
+                <TextInput source='text' validate={[required()]} label='Text'></TextInput>
+                <BooleanInput source='correct' label='Correct option' />
+                <ReferenceInput source='challengeId' reference='challenges' />
+                <TextInput source='imageSrc' label='Image URL'/>
+                <TextInput source='audioSrc' label='Audio URL'/>
+            </SimpleForm>
+        </Edit>
+    );
+};
